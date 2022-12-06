501084f641931f2d2a485fe4b29fb4324c347b60
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-12-06 13:46:17 +0400

    adding images to system

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ app.use(session({
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
