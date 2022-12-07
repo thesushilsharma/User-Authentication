191e23ac54e9c146f3769a644952177562ac701e
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-12-07 11:16:40 +0400

    hints

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/Hints', express.static(__dirname + 'public/Hints'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
